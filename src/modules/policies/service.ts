8ec31d9243d731b4503596b7721d5528ac9c8ea3
pesolwork <pesol2012.work@gmail.com>
2025-12-05 21:06:41 +0700

    Update service.ts
    fix invalid query key

--- src/modules/policies/service.ts
+++ src/modules/policies/service.ts
@@ -33,7 +33,7 @@ export class PolicyService extends BaseService<Policy, PolicyDTO> {
       };
     }
 
-    const fields = ['policyId', 'customerId'];
+    const fields = ['planId', 'customerId'];
     for (const value of fields) {
       if (query[value]) {
         options.where[value] = query[value];
