6ecf11411be9894335cb12f0b17cb548a91978c1
pesolwork <pesol2012.work@gmail.com>
2025-12-06 11:38:30 +0700

    feat : AuthService join customer

--- src/modules/customers/service.ts
+++ src/modules/customers/service.ts
@@ -14,6 +14,13 @@ import { UserRole } from 'src/common/enum';
 
 @Injectable()
 export class CustomerService extends BaseService<Customer, CustomerDTO> {
+  constructor(
+    private readonly _repository: CustomerRepository,
+    private readonly usersService: UserService,
+  ) {
+    super(_repository);
+  }
+
   async getProfile(userId: number): Promise<ResponseDTO<CustomerDTO>> {
     const data = await this._repository.findOne({
       where: {
@@ -26,12 +33,6 @@ export class CustomerService extends BaseService<Customer, CustomerDTO> {
     }
 
     return new ResponseDTO({ data: new CustomerDTO(data) });
-  }
-  constructor(
-    private readonly _repository: CustomerRepository,
-    private readonly usersService: UserService,
-  ) {
-    super(_repository);
   }
 
   async register(body: CreateCustomerDTO): Promise<ResponseDTO<CustomerDTO>> {
